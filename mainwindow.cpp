93102acd2570f454d239b8be974dc3e35e15d192
Maciej Dems <maciej.dems@p.lodz.pl>
2021-11-14 18:38:29 +0100

    Status display

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,10 +23,11 @@ MainWindow::MainWindow()
     nextAction = new QAction(QIcon::fromTheme("media-skip-forward"), tr("&Next Track"), this);
     connect(nextAction, &QAction::triggered, this, &MainWindow::next);
 
-    playAction = new QAction(QIcon::fromTheme("media-playback-start"), tr("&Play"), this);
+    playAction = new QAction(QIcon::fromTheme("media-playback-start"), tr("P&lay"), this);
     connect(playAction, &QAction::triggered, this, &MainWindow::play);
 
     pauseAction = new QAction(QIcon::fromTheme("media-playback-pause"), tr("&Pause"), this);
+    pauseAction->setVisible(false);
     connect(pauseAction, &QAction::triggered, this, &MainWindow::pause);
 
     trayIconMenu = new QMenu(this);
@@ -37,6 +38,10 @@ MainWindow::MainWindow()
 
     trayIconMenu->addSeparator();
     trayIconMenu->addAction(quitAction);
+
+    QDBusConnection::sessionBus().connect(nullptr, "/org/mpris/MediaPlayer2", "org.freedesktop.DBus.Properties",
+                                          "PropertiesChanged", this,
+                                          SLOT(dbusPropertiesChanged(QString, QVariantMap, QStringList)));
 
     trayIcon->setToolTip(tr("Spotify"));
     trayIcon->setContextMenu(trayIconMenu);
@@ -74,3 +79,59 @@ void MainWindow::next() { dbus.call("Next"); }
 void MainWindow::play() { dbus.call("Play"); }
 
 void MainWindow::pause() { dbus.call("Pause"); }
+
+void MainWindow::dbusPropertiesChanged(const QString& name, const QVariantMap& properties, const QStringList&) {
+    if (name == "org.mpris.MediaPlayer2.Player") {
+        if (properties.contains("PlaybackStatus")) {
+            QString status = properties["PlaybackStatus"].toString();
+            if (status == "Playing") {
+                playAction->setVisible(false);
+                pauseAction->setVisible(true);
+            } else {
+                playAction->setVisible(true);
+                pauseAction->setVisible(false);
+            }
+        }
+
+        if (properties.contains("Metadata")) {
+            QVariantMap metadata;
+
+            if (properties["Metadata"].type() == QVariant::Map) {
+                metadata = properties["Metadata"].toMap();
+            } else {
+                properties["Metadata"].value<QDBusArgument>() >> metadata;
+            }
+
+            // for (QVariantMap::const_iterator it = metadata.cbegin(), end = metadata.cend(); it != end; ++it) {
+            //     qDebug() << "metadata: " << it.key() << " value: " << it.value();
+            // }
+
+            QString title = metadata["xesam:title"].toString();
+            QString artist = metadata["xesam:artist"].toStringList().join(", ");
+            // QString album = metadata["xesam:album"].toString();
+
+            if (title.isEmpty()) {
+                title = artist;
+                artist = "";
+            }
+
+            if (!title.isEmpty()) {
+                if (!artist.isEmpty())
+                    trayIcon->setToolTip(tr("Spotify (%1 - %2)").arg(title).arg(artist));
+                else
+                    trayIcon->setToolTip(tr("Spotify (%1)").arg(title));
+            } else {
+                trayIcon->setToolTip(tr("Spotify"));
+            }
+
+            // QString cover = metadata["mpris:artUrl"].toString();
+            // if (!cover.isEmpty()) {
+            //     QPixmap pixmap;
+            //     pixmap.loadFromData(QByteArray::fromBase64(cover.toUtf8()));
+            //     trayIcon->setIcon(pixmap);
+            // } else {
+            //     trayIcon->setIcon(QIcon::fromTheme("spotify", QIcon("/usr/share/pixmaps/spotify-client.png")));
+            // }
+        }
+    }
+}
